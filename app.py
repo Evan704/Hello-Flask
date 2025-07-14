639af94f11a733d0c148b005250abf2ad6072f5a
Evan704 <liuyf24@mails.tsinghua.edu.cn>
2025-07-14 15:31:14 +0800

    add template and some basic pages

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # 创建一个 Flask 应用实例
 app = Flask(__name__)
 
 # 定义一个路由和视图函数
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 # 当直接运行这个脚本时，启动开发服务器
 if __name__ == '__main__':
